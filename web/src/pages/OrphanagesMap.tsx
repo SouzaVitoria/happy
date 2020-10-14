b172a91af69442118216bf73b11306205e19b620
Vitória Souza <vitoria.souza@acct.global>
2020-10-14 11:26:05 -0300

    changed openstreetmap by mapbox

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -26,7 +26,7 @@ export default function OrphanagesMap(){
        zoom={17}
        style={{width: "100%", height: "100%"}}
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
       </Map>
       
       <Link to="" className="create-orphanage">
