4e4779dc709d4e60c1faa484efe8df061355da80
Vitória Souza <vitoria.souza@acct.global>
2020-10-13 23:23:51 -0300

    Added landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import {FiArrowRight} from "react-icons/fi";
 
-interface TitleProps {
-  text: string;
-}
-
-function Title(props: TitleProps){
-  return <h1>{props.text}</h1>
-}
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+import logoImg from "./images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-     <Title text="1 - Hello "/>
-     <Title text="2 - Hello "/>
-     <Title text="3 - Hello "/>
+    <div id="page-landing">
+    <div className="content-wrapper">
+      <img src={logoImg} alt=""/>
+
+      <main>
+        <h1>Leve felicidade para o mundo</h1>
+        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+      </main>
+
+      <div className="location">
+        <strong>São Paulo</strong>
+        <span>Nazaré Paulista</span>
+      </div>
+
+      <a href="#" className="enter-app">
+         <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/> 
+      </a>
+
+    </div>
     </div>
   );
 }
