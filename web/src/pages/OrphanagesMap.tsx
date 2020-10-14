2f15a5ae0d71437dd993bf9a506e17cfefbbb94b
Vitória Souza <vitoria.souza@acct.global>
2020-10-14 11:17:12 -0300

    Added map with Leaflet

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
+import { Map, TileLayer } from "react-leaflet";
 import mapMarkerImg from "../images/map-marker.svg";
 import "../styles/pages/orphanages-map.css"; 
+import "leaflet/dist/leaflet.css";
 
 export default function OrphanagesMap(){
   return(
@@ -18,7 +20,15 @@ export default function OrphanagesMap(){
          <span>Nazaré Paulista</span>
         </footer>
       </aside>
-      <div></div>
+
+      <Map
+       center={[-23.181428,-46.4033447]}
+       zoom={17}
+       style={{width: "100%", height: "100%"}}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+      
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
