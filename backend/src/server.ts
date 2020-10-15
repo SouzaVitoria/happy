ec1000c920b7fa407bb25dda642bff27298a87d6
Vitória Souza <vitoria.souza@acct.global>
2020-10-15 09:33:20 -0300

    Added orphanages in database - post

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,36 @@
 import express from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "./models/Orphanage";
 import "./database/connection";
 
 const app = express();
 app.use(express.json());
 
-app.get("/users", (request, response) => {
+app.post("/orphanages", async (request, response) => {
+  console.log(request.body);
+  const {
+    name,
+    latitude,
+    longitude, 
+    about, 
+    instructions, 
+    opening_hours, 
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude, 
+    about, 
+    instructions, 
+    opening_hours, 
+    open_on_weekends
+  }); 
+
+  await orphanagesRepository.save(orphanage);
+
   return response.json({ message: "Hello World" });
 });
 
