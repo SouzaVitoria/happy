673b91733a055504c27a54fd261ec3069acae6f3
Vitória Souza <vitoria.souza@acct.global>
2020-10-15 10:04:45 -0300

    added listing of orphanages and details - GET

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,13 @@ import { getRepository } from "typeorm";
 import Orphanage from "../models/Orphanage";
 
 export default {
+
+  async index(request: Request, response: Response){
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanages = await orphanagesRepository.find();
+    return response.json(orphanages);
+  },
+
   async create(request: Request, response: Response) {
     const {
       name,
@@ -29,5 +36,12 @@ export default {
     await orphanagesRepository.save(orphanage);
   
     return response.status(201).json(orphanage);
+  },
+
+  async show(request: Request, response: Response){
+    const { id } = request.params;
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    return response.json(orphanage);
   }
 }
